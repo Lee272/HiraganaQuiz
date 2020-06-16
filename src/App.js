07f603049e6e130a136837807b3806b293dd49f9
Lee Ritson <37814016+Lee272@users.noreply.github.com>
2020-06-16 15:19:56 +0100

    fixed results

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import ImagePlaceHolder from './Components/ImagePlaceHolder';
 
 export default function App() {
 // States
-const [textValue, settextValue] = useState('');
+const [correctCount, setcorrectCount] = useState(0);
+const  [wrongCount, setwrongCount] = useState(0);
+
+// const [textValue, settextValue] = useState('');
 
 // Functions
-//Get Data from Textbox & Verify it
+
+// Get Data from Textbox & Verify it
 const dataResult = (event) => {
   event.preventDefault();
   const dataSearched = event.target.searchText.value
   .toLowerCase()
   .trim();
-  settextValue(dataSearched);
+  // settextValue(dataSearched);
 
-    if (dataSearched === '1') {
+  let answer = document.querySelector(".ans").innerHTML;
+
+    if (dataSearched === answer) {
       return (console.log("True"))
     } else {
       return (console.log('Failed'))
@@ -26,7 +32,9 @@ const dataResult = (event) => {
     <div className="w-100">
 
       <div className="ba w-50 tc center">
-        <ImagePlaceHolder />
+        <div>
+          <ImagePlaceHolder />
+        </div>
       </div>
 
       <form onSubmit={event => {dataResult(event)}}>
--- src/Components/ImagePlaceHolder.jsx
+++ src/Components/ImagePlaceHolder.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function ImagePlaceHolder() {
 //Arrays
-const Data = ['Ichi', 'Ni', 'San', 'Yon', 'Go', 'Rokku']
+const Data = ['ichi', 'ni', 'san', 'yon', 'go', 'rokku']
 //Functions
 const randomResult = 
     Data[Math.floor(Math.random()*Data.length)]
 
     return(
         <div>
-            <p>{randomResult}</p>
+            <p className="ans ttc">{randomResult}</p>
         </div>
     )
 }
