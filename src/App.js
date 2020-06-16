7c572b0b8e1d984dbde96c301ffddd7b6dcb3522
Lee Ritson <37814016+Lee272@users.noreply.github.com>
2020-06-16 14:18:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ export default function App() {
 // States
 const [textValue, settextValue] = useState('');
 
-//Arrays
-const Data = ['Ichi', 'Ni', 'San', 'Yon', 'Go', 'Rokku']
-
 // Functions
-//Get Data from Textbox
+//Get Data from Textbox & Verify it
 const dataResult = (event) => {
   event.preventDefault();
   const dataSearched = event.target.searchText.value
@@ -24,17 +21,6 @@ const dataResult = (event) => {
       return (console.log('Failed'))
     }
   }
-//Verify Data from Textbox
-// const dataAnswer = () => {
-//   if (textValue === '1') {
-//     return (console.log("True"))
-//   } else {
-//     return (console.log('Failed'))
-//   }
-// }
-//Randomizer
-const randomResult = 
-    Data[Math.floor(Math.random()*Data.length)]
 
   return (
     <div className="w-100">
