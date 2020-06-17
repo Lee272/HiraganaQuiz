c0139da5be611ed0d453ffd891d83aa8b7e0cd4b
Lee Ritson <37814016+Lee272@users.noreply.github.com>
2020-06-17 11:42:40 +0100

    Array to Object Changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React, { useState } from 'react';
 import './App.css';
-import ImagePlaceHolder from './Components/ImagePlaceHolder';
+// import ImagePlaceHolder from './Components/ImagePlaceHolder';
 import Card from './Components/Card';
 
 export default function App() {
+// Variables
+
+// Objects
+  const dataObject = [
+    {
+        name: 'ho',
+        image: 'http://www.dartmouth.edu/~introjpn/text/hiragana/hiragana_files/30_ho.jpg'
+    },
+    {
+        name: 'mo',
+        image: 'http://www.dartmouth.edu/~introjpn/text/hiragana/hiragana_files/35_mo.jpg'
+    },
+    {
+        name: 'n',
+        image: 'http://www.dartmouth.edu/~introjpn/text/hiragana/hiragana_files/46_n.jpg'
+    },
+    {
+        name: 'no',
+        image: 'http://www.dartmouth.edu/~introjpn/text/hiragana/hiragana_files/25_no.jpg'
+    },
+    {
+        name: 'ro',
+        image: 'http://www.dartmouth.edu/~introjpn/text/hiragana/hiragana_files/43_ro.jpg'
+    },
+    {
+        name: 'to',
+        image: 'http://www.dartmouth.edu/~introjpn/text/hiragana/hiragana_files/20_to.jpg'
+    },
+    {
+        name: 'yo',
+        image: 'http://www.dartmouth.edu/~introjpn/text/hiragana/hiragana_files/38_yo.jpg'
+    }
+]
+// Arrays
+const Data = ['mo', 'ni', 'san', 'yon', 'go', 'rokku'];
 // States
 const [correctCount, setcorrectCount] = useState(0);
 const  [wrongCount, setwrongCount] = useState(0);
-
-// const [textValue, settextValue] = useState('');
-
 // Functions
+
+// Randomizer - Array
+const randomResult = 
+    Data[Math.floor(Math.random()*Data.length)];
+// Randomizer - Object
+const prep = Math.floor(Math.random()*dataObject["length"])
+const randomResult2 = dataObject[prep]["name"]
 
 // Get Data from Textbox & Verify it
 const dataResult = (event) => {
@@ -18,11 +57,8 @@ const dataResult = (event) => {
   const dataSearched = event.target.searchText.value
   .toLowerCase()
   .trim();
-  // settextValue(dataSearched);
 
-  let answer = document.querySelector(".ans").innerHTML;
-
-    if (dataSearched === answer) {
+    if (dataSearched === randomResult2) {
       return (setcorrectCount(correctCount + 1))
     } else {
       return (setwrongCount(wrongCount + 1))
@@ -31,25 +67,23 @@ const dataResult = (event) => {
 
   return (
     <div className="w-100">
-
-      <div className="ba w-50 tc center">
-        <div>
-          <ImagePlaceHolder />
-        </div>
-      </div>
-
-      <form onSubmit={event => {dataResult(event)}}>
-        <div class="fl w-100 w-50-ns pa2">
-          <input type="text" name="searchText" />
-          <button className="ma2">Next</button>
-        </div>
-      </form>
-
       <div>
-        <p>Correct Answers: {correctCount}</p>
-        <p>Wrong Answers: {wrongCount}</p>
+        <div className="tc">
+            <Card name={randomResult2} hira={dataObject[prep]["image"]} />
+        </div>
+
+        <form onSubmit={event => {dataResult(event)}}>
+          <div class="ma2">
+            <input type="text" name="searchText" />
+            <button className="ma2">Next</button>
+          </div>
+        </form>
+
+        <div className="ma2">
+          <p>Correct Answers: {correctCount}</p>
+          <p>Wrong Answers: {wrongCount}</p>
+        </div>
       </div>
-      <Card />
     </div>
   );
 }
--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import ho from '../Images/ho.png'
 
-export default function Card() {
-const dataObject = [
-    {
-        name: 'ho',
-        image: '../Images/ho.png'
-    },
-    {
-        name: 'mo',
-        image: '../Images/mo.png'
-    },
-    {
-        name: 'n',
-        image: '../Images/n.png'
-    },
-    {
-        name: 'no',
-        image: '../Images/no.png'
-    },
-    {
-        name: 'ro',
-        image: '../Images/ro.png'
-    },
-    {
-        name: 'to',
-        image: '../Images/to.png'
-    },
-    {
-        name: 'yo',
-        image: '../Images/yo.png'
-    }
-]
+export default function Card(props) {
 // Arrays
 const Data = ['mo', 'ni', 'san', 'yon', 'go', 'rokku'];
 
   return(
     <div>
-      <article class="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+      <article class="mw5 ma2 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
 			  <div class="tc">
-    			<img src={ho} class="br-100 h3 w3 dib" title="hiragana-ho" alt="img" />
-    			<h1 class="f4 ttc">{Data[0]}</h1>
+    			<img src={props.hira} class="br-100 h3 w3 dib" title="hiragana-ho" alt="img" />
+    			<h1 class="f4 ttc">{props.name}</h1>
     			<hr class="mw3 bb bw1 b--black-10" />
   			</div>
 			</article>
