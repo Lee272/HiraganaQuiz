aa2eeb5a18e747a7d2fd2beb845826cd627275e3
Lee Ritson <37814016+Lee272@users.noreply.github.com>
2020-06-30 23:38:12 +0100

    Added Toggle for Katakana

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import "./style.css";
 import Card from "./Components/Card";
 import Timer from "./Components/Timer";
 import Hiragana from "./Components/Data/Data.json";
-// import Katakana from './Components/Data/Katakana.json';
+import Katakana from "./Components/Data/Katakana.json";
 
 export default function App() {
   // Variables
-  const dataObject = Hiragana;
+  const dataObjectHira = Hiragana;
+  const dataObjectKata = Katakana;
   // States
   const [correctCount, setcorrectCount] = useState(0);
   const [wrongCount, setwrongCount] = useState(0);
-  const [wip, setwip] = useState(false);
+  const [hiraganaState, setHiraganaState] = useState(true);
+  const [KatakanaState, setKatakanaState] = useState(false);
   const [correct, setcorrect] = useState(false);
 
   // Functions
 
-  // Randomizer - Object
-  const prep = Math.floor(Math.random() * dataObject["length"]);
-  const randomResult2 = dataObject[prep]["name"];
+  // Randomizer - Object Hiragana
+  const prepHira = Math.floor(Math.random() * dataObjectHira["length"]);
+  const randomResult2 = dataObjectHira[prepHira]["name"];
+  // Randomizer - Object Katakana
+  const prepKata = Math.floor(Math.random() * dataObjectKata["length"]);
+  const randomResult3 = dataObjectKata[prepKata]["name"];
 
   // Get Data from Textbox & Verify it
   const dataResult = (event) => {
@@ -27,7 +32,7 @@ export default function App() {
     const dataSearched = event.target.searchText.value.toLowerCase().trim();
     event.target.reset();
 
-    if (dataSearched === randomResult2) {
+    if (dataSearched === randomResult3) {
       return setcorrectCount(correctCount + 1);
     } else {
       return setwrongCount(wrongCount + 1);
@@ -41,12 +46,31 @@ export default function App() {
     }
   };
 
+  // Writing Change
+  const WritingToggle = (event) => {
+    event.preventDefault();
+    setHiraganaState(!hiraganaState);
+    setKatakanaState(!KatakanaState);
+  };
+
   return (
     <div className="w-100">
+      {console.log("Katakana", dataObjectKata)}
       <h1 className="tc calisto">Hiragana Quiz</h1>
       <div>
         <div className="tc">
-          <Card name={randomResult2} hira={dataObject[prep]["image"]} />
+          {hiraganaState && (
+            <Card
+              name={randomResult2}
+              hira={dataObjectHira[prepHira]["image"]}
+            />
+          )}
+          {KatakanaState && (
+            <Card
+              name={randomResult3}
+              hira={dataObjectKata[prepKata]["image"]}
+            />
+          )}
         </div>
 
         <form
@@ -69,8 +93,17 @@ export default function App() {
           </p>
         </div>
         <div className="tc calisto">
-          <button onClick={() => setwip(true)}>Switch to Katakana</button>
-          {wip && <p className="red">Under Construction!</p>}
+          {hiraganaState && (
+            <button onClick={(event) => WritingToggle(event)}>
+              Switch to Katakana
+            </button>
+          )}
+          {KatakanaState && (
+            <button onClick={(event) => WritingToggle(event)}>
+              Switch to Hiragana
+            </button>
+          )}
+          {/* {KatakanaState && <p className="red">Under Construction!</p>} */}
         </div>
         <div className="tc ma3">
           <button onClick={correct1}>Timer</button>
