4b64359fb98f53f8dafef6ba167c42dbb3389a65
Lee Ritson <37814016+Lee272@users.noreply.github.com>
2020-08-20 14:30:41 +0100

    Add files via upload

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-export default function Timer(){
-// States
-const [counter, setCounter] = React.useState(20);
+export default function Timer({ parentCall }) {
+  // States
+  const [counter, setCounter] = useState(30);
+  const [end, setend] = useState(false);
 
-// Functions
-React.useEffect( () => { 
-	counter > 0 && setTimeout( () => setCounter(counter - 1), 1000);
-}, [counter]);
+  //Effect
+  // Functions
+  React.useEffect(() => {
+    if (counter === 0) {
+      setend(true);
+      parentCall();
+    } else {
+      counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    }
+  }, [counter, parentCall]);
 
-  return(
-    <div className="tc ma3">
-    	Countdown: {counter}
+  return (
+    <div>
+      <div className="text-center ma3">Countdown: {counter}</div>
+      {end && <h1 className="calisto">Stop!</h1>}
     </div>
-  )
+  );
 }
